2bbe8721e56da36203bde55199838b896856a94c
João Daniel <jotaf.daniel@gmail.com>
2022-01-06 17:51:55 -0300

    nerf labeled input
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- src/components/utils/LabeledInput.jsx
+++ src/components/utils/LabeledInput.jsx
@@ -1,16 +1,12 @@
 import './LabeledInput.css'
 
-function LabeledInput({ id, value, onChange, label, error, type }) {
-  const hasError = error?.length > 0
-  const errorMessage = hasError ? error : ''
-
+function LabeledInput({ label, type }) {
   return (
     <div className="input-control">
-      <label htmlFor={id}>
+      <label>
         {label}
       </label>
-      <input type={type} className={hasError ? "error" : ''} id={id} value={value} onChange={onChange} />
-      { hasError && <p className="error">{errorMessage}</p>}
+      <input type={type} />
     </div>
   )
 }
